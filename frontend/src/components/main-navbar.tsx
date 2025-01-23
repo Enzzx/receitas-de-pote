19f54d1d3cf988383d0dccec159048bb61b23c03
Enzzx <bellottie815@gmail.com>
2025-01-23 05:48:02 -0300

    change main-navbar

--- frontend/src/components/main-navbar.tsx
+++ frontend/src/components/main-navbar.tsx
@@ -4,11 +4,11 @@ export default function MainNavbar() {
     return (
         <ul>
             <li><NavLink to="/"><img src="/" alt="Logo" /></NavLink></li>
-            <li><NavLink to="/list" end><img src="./icons/to-do.png" alt="to-do list" /> Lista</NavLink></li>
-            <li><NavLink to="/news" end><img src="./icons/to-do.png" alt="news" /> News</NavLink></li>
-            <li><NavLink to="/" end><img src="./icons/home.png" alt="home" /> Home</NavLink></li>
-            <li><NavLink to="/profile" end><img src="./icons/user.png" alt="profile" /> Conta</NavLink></li>
-            <li><NavLink to="/about" end><img src="./icons/to-do.png" alt="about" /> Sobre</NavLink></li>
+            <li><NavLink to="/list" end><img src="./icons/to-do.png" alt="to-do list" /> <span>Lista</span></NavLink></li>
+            <li><NavLink to="/news" end><img src="./icons/to-do.png" alt="news" /> <span>News</span></NavLink></li>
+            <li><NavLink to="/" end><img src="./icons/home.png" alt="home" /> <span>Home</span></NavLink></li>
+            <li><NavLink to="/profile" end><img src="./icons/user.png" alt="profile" /> <span>Conta</span></NavLink></li>
+            <li><NavLink to="/about" end><img src="./icons/to-do.png" alt="about" /> <span>Sobre</span></NavLink></li>
         </ul>
     )
 }
