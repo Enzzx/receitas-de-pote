0f3f210fc929770ccf8cb55e5853efb82208ec06
Enzzx <bellottie815@gmail.com>
2025-02-05 17:51:03 -0300

    start server to client communication

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Navbar from "../components/navbar"
 import HomeNews from "../components/home-news"
 import RecipesContainer from "../components/recipes-container"
@@ -7,6 +8,27 @@ import meals from "../data-sim/recipes-meals.json"
 import pasta from "../data-sim/recipes-pasta.json"
 
 export default function Home() {
+    const [backHi, setBackHi] = useState<any>(null)
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        async function serverHi() {
+            try {
+                const req = await fetch(import.meta.env.VITE_BACKEND_URL+"/")
+                const res = await req.text()
+                setBackHi(res)
+            } catch (e) {
+                console.log(e)
+                setBackHi("sem hi :(")
+            } finally {
+                setLoading(false)
+            }
+        }
+        setTimeout(() => {
+            serverHi()
+        }, 3000)
+    }, [])
+
     return (
         <>
             <Navbar />
@@ -16,7 +38,7 @@ export default function Home() {
             <div className="img-background">
                 <section></section>
                 <article>
-                    <h2>Title something</h2>
+                    <h2>{loading ? "Tá vindo..." : backHi}</h2>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo dicta nulla non quae labore repellat facilis nesciunt quod doloribus, totam sit.</p>
                 </article>
             </div>
