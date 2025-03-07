5f7194cb28ff8523d60330db4184329bcbe3bf75
zenzx <bellottie815@gmail.com>
2025-03-07 05:54:38 -0300

    setting environment variable

--- backend/middleware/cors.go
+++ backend/middleware/cors.go
@@ -2,12 +2,19 @@ package middleware
 
 import (
 	"net/http"
+	"os"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	_ = godotenv.Load() // Carrega o .env automaticamente
+}
 
 // add CORS verifications to all endpoints
 func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		origin := os.Getenv("CORS_ORIGINS")
+		w.Header().Set("Access-Control-Allow-Origin", origin)
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
