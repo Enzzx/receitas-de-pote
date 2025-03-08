5f358b8933ed1445f8b7268d68681d8042a0086b
zenzx <bellottie815@gmail.com>
2025-03-08 03:55:15 -0300

    add log-out and delete functions to buttons on account page

--- frontend/src/components/profile-info.tsx
+++ frontend/src/components/profile-info.tsx
@@ -1,7 +1,37 @@
-import { UserProfile } from "../models"
+import { useNavigate } from "react-router-dom"
+import Cookies from "js-cookie"
+import { UserProfile, HttpAccBody } from "../models"
 
 export default function ProfileInfo(props: UserProfile) {
     const { img, username, recipes, news } = props
+    const navigate = useNavigate()
+
+    function clearLogin() {
+        sessionStorage.clear()
+        Cookies.remove("jwt")
+        navigate("/")
+    }
+
+    async function deleteAcc() {
+        const head = {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(sessionStorage.getItem("username"))
+        }
+
+        try {
+            const req = await fetch(import.meta.env.VITE_BACKEND_URL + "/user/delete", head)
+            const res: HttpAccBody = await req.json()
+
+            if (res.Succesfull) {
+                alert(res.Message)
+                clearLogin()
+            }
+
+        } catch (e) {
+            throw e
+        }
+    }
 
     return (
         <div id="profile-info">
@@ -15,8 +45,8 @@ export default function ProfileInfo(props: UserProfile) {
                 </section>
             </section>
             <div id="profile-sys">
-                <button>sair da conta</button>
-                <button>apagar conta</button>
+                <button onClick={clearLogin}>sair da conta</button>
+                <button onClick={deleteAcc}>apagar conta</button>
             </div>
         </div>
     )
--- frontend/src/pages/account.tsx
+++ frontend/src/pages/account.tsx
@@ -2,10 +2,17 @@ import { Outlet } from "react-router-dom"
 import AccountNavbar from "../components/account-navbar"
 
 export default function Account() {
+    const isLogged = typeof(sessionStorage.getItem("username")) == typeof("a") ? true : false
     return (
-        <>
-            <AccountNavbar />
-            <Outlet />
+        <> 
+        {
+            isLogged ? 
+            <>
+                <AccountNavbar />
+                <Outlet />
+            </>
+            : <div>conecte se em uma cobnta</div>
+        }
         </>
     )
 }
