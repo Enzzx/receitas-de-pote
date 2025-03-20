d8e3c71852faa6845050ef237f1bc3b07ac55818
zenzx <bellottie815@gmail.com>
2025-03-20 16:52:15 -0300

    change pgx's scheme connection to pool

--- backend/config/db.go
+++ backend/config/db.go
@@ -1,16 +1,17 @@
 package config
 
 import (
-    "context"
-    "fmt"
-    "log"
-    "os"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-    "github.com/joho/godotenv"
-    "github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
 )
 
-var DB *pgx.Conn
+var DB *pgxpool.Pool
 
 // open connection to postgresSQL
 func ConnectDB() {
@@ -24,7 +25,14 @@ func ConnectDB() {
 
     dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
 
-    conn, err := pgx.Connect(context.Background(), dsn)
+    config, _ := pgxpool.ParseConfig(dsn)
+    config.MaxConns = 10
+    config.MinConns = 2
+    config.MaxConnLifetime = time.Hour
+    config.MaxConnIdleTime = 30 * time.Minute
+    config.ConnConfig.ConnectTimeout = 10 * time.Second
+
+    conn, err := pgxpool.NewWithConfig(context.Background(), config)
     if err != nil {
         log.Fatalf("erro ao conectar ao banco de dados: %v", err)
     }
@@ -35,6 +43,6 @@ func ConnectDB() {
 
 // end connection
 func CloseDB() {
-	DB.Close(context.Background())
+	DB.Close()
 	log.Println("Conexão com o banco fechada")
 }
